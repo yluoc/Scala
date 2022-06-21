5576fc8c0f5b3d5de2c8b7b185fa6d0da20f8dcd
yluoc <98978059+yluoc@users.noreply.github.com>
2022-06-21 00:19:33 -0400

    Update and rename hw_2_test_case to hw_2_test_case.scala
    The test function is quite easy, just question_2/check2 is a little bit tricky for me.

--- hw_2_test_case.scala
+++ hw_2_test_case.scala
@@ -73,3 +73,48 @@ object question_2 extends App{
     case _ => "other type of data"
   }
 }
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class hw_2_test_ques1 extends AnyFunSuite {
+  test("question_1.positive testcase") {
+    assert(question_1.check1("welcome") === "wweellccoommee")
+    assert(question_1.check1("cse") === "ccssee")
+  }
+  test("question_1.negative testcase") {
+    assert(question_1.check1(1) === "the input is an integer")
+    assert(question_1.check1(2.0) === "the input is a double")
+    assert(question_1.check1(1f) === "the input is a float")
+    assert(question_1.check1('c') === "other type of data")
+    assert(question_1.check1("") === null)
+    assert(question_1.check1("   ") === null)
+  }
+}
+
+class hw_2_test_ques2 extends AnyFunSuite {
+  test("question_2.positive testcase") {
+    assert(question_2.check2(Array(2, 3, 5, 7, -7, 5, 8, -5)) === 56)
+    assert(question_2.check2(Array(2, 3, 5, 9, -7, 5, 9, -5 )) === 81)
+  }
+  test("question_2.negative testcase") {
+    assert(question_2.check2(Array(2.0,3.0,4.0,5.0)) === "the input is a double")
+    assert(question_2.check2(Array(2f, 3f, 5f, 9f, 7d)) === "the input is a float")
+    assert(question_2.check2(Array("1","2","2")) === "the input is a string")
+    assert(question_2.check2(Array('c','d','f')) === "other type of array")
+    assert(question_2.check2(Array()) === null)
+    assert(question_2.check2(Array(  )) === null)
+  }
+}
+
+class hw_2_test_ques3 extends AnyFunSuite {
+  test("question_3.positive testcase") {
+    assert(question_3.check3("it 15 is25 a 20string") === 60)
+    assert(question_3.check3("She is 22 yr old 5") === 27)
+  }
+  test("question_3.negative testcase") {
+    assert(question_3.check3(1) === "the input is an integer")
+    assert(question_3.check3(1.0) === "the input is a double")
+    assert(question_3.check3(1f) === "the input is a float")
+    assert(question_3.check3('c') === "other type of data")
+  }
+}
